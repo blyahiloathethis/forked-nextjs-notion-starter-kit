8c8032e5fe4e6be6858b4aabd6b54856b9dcdc5e
Sehnsucht <jadebang69@gmail.com>
2026-02-25 03:59:39 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,6 +11,7 @@ export default {
       { protocol: 'https', hostname: 'abs.twimg.com' },
       { protocol: 'https', hostname: 'pbs.twimg.com' },
       { protocol: 'https', hostname: 's3.us-west-2.amazonaws.com' },
+      { protocol: 'https', hostname: 's3-us-west-2.amazonaws.com' },
       { protocol: 'https', hostname: 'img.notionusercontent.com' }
     ],
     formats: ['image/avif', 'image/webp'],
